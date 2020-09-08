e7f8e7f5aa7fed6770c420853a09c8d011b07c89
Oscar Argueta <aradevs.argueta@gmail.com>
2020-09-08 14:28:41 -0600

    Added first example with additional fields

---
+++ guia04/crudAngular/src/app/models/alumno.ts
@@ -0,0 +1,9 @@
+export class Alumno {
+    id: number = 0;
+    name: string ='';
+    lastname:string = '';
+    age: number = 0;
+    address: string = '';
+    phone: string=  '';
+    email: string = '';
+}
